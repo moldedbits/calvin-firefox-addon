13a7ccf6e122a5bc743afa008b35c4c76d0a5467
Amit Kumar Swami <amitiitm2009@gmail.com>
2016-01-11 20:54:42 +0530

    getting url from calvin-backend and reloading to a pinned tab

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 var self = require('sdk/self');
+var { getTabForId, setTabURL } = require ("sdk/tabs/utils");
+var tabID = "calvin-bot-tab";
+var tabs = require("sdk/tabs");
+var { setInterval } = require("sdk/timers");
+var delay = 1000 * 60
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+tabs.open({
+  url: "http://www.moldedbits.com",
+  onReady: function onReady(tab) {
+  },
+  isPinned: true,
+  onActivate: function onActivate(tab) {
+    reloadURLFromBackend()
+    tabID = tab.id;
+  }
+});
+
+function reloadURLFromBackend() {
+    var Request = require("sdk/request").Request;
+    var randomURL = Request({
+      url: "http://calvin-backend.herokuapp.com/data/get_random_url.json",
+      onComplete: function (response) {
+        var newURL = response.json.url;
+        console.log("success with URL:- " + newURL);
+        openRandomURL(newURL);
+      }
+    }).get();
 }
 
-exports.dummy = dummy;
+function openRandomURL(url) {
+  setTabURL(getTabForId(tabID), url)
+}
+
+setInterval(function() {
+  reloadURLFromBackend()
+}, delay)
